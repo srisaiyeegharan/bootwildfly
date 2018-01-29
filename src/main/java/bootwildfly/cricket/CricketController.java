d684a004c7216698a4fd0d5bc99c1578dc81dc82
Srisaiyeegharan Kidnapillai <100680347@student.swin.edu.au>
2018-01-29 22:05:00 +1100

    Addes business service

--- src/main/java/bootwildfly/cricket/CricketController.java
+++ src/main/java/bootwildfly/cricket/CricketController.java
@@ -1,15 +1,23 @@
 package bootwildfly.cricket;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CricketController {
 	
+	private CricketService cricketService;
+	
 	@RequestMapping("/clubs")
 	public String listClubs()
 	{
-		return "This will list all the clubs soon";
+		return cricketService.getAllClubs();
 	}
 	
+	@RequestMapping("/clubs/{id}")
+	public String getClub(@PathVariable String id)
+	{
+		return cricketService.getSingleClub(id);
+	}
 }
---
+++ src/main/java/bootwildfly/cricket/CricketService.java
@@ -0,0 +1,14 @@
+package bootwildfly.cricket;
+
+public class CricketService {
+	
+	public String getAllClubs()
+	{
+		return "This will list all the clubs soon";
+	}
+	
+	public String getSingleClub(String id)
+	{
+		return "This will list your chosen club soon: " + id;
+	}
+}
