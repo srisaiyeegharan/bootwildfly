6ef128840e1b6596ecc6fb7fdcb1638ff60118f5
Srisaiyeegharan Kidnapillai <100680347@student.swin.edu.au>
2018-01-29 22:14:41 +1100

    Modified business service with autowired annotation

--- src/main/java/bootwildfly/cricket/CricketController.java
+++ src/main/java/bootwildfly/cricket/CricketController.java
@@ -1,5 +1,6 @@
 package bootwildfly.cricket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CricketController {
 	
+	@Autowired
 	private CricketService cricketService;
 	
 	@RequestMapping("/clubs")
